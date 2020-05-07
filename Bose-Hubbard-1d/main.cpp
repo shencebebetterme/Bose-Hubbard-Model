bc27c08b25efff4555baf8f95cde2ec862257dfe
ce shen <scbebetterme@gmail.com>
2020-05-07 21:52:21 +0800

    use hdf5 dynamic linking

--- Bose-Hubbard-1d/main.cpp
+++ Bose-Hubbard-1d/main.cpp
@@ -1,8 +1,11 @@
-
+/*
+in order to use armadillo hdf5 save/load functionality
+hdf5.h must be available ==> additional  include dir hdf5/include/
+link with the hdf5 library:
+		static linking ==> libszip.lib;libzlib.lib;libhdf5.lib;libhdf5_cpp.lib;
+		dynamic linking ==> szip.lib;zlib.lib;hdf5.lib;hdf5_cpp.lib; 
+*/
 #include "pch.h"
-//#include "hdf5.h"
-//#include "H5Cpp.h"
-//
 #include "BHModel.h"
 
 const int numSites = 5;
@@ -27,5 +30,7 @@ int main() {
 	//data.print("data:");
 	arma::Mat<int> data;
 	data.load(arma::hdf5_name("data.h5", "Dataset1"));
+	//because h5 is column based, we need to take the transpose
+	data = data.t();//now data is a nBasis*basisLen matrix
 	data.print("data matrix is:");
 }
--- Bose-Hubbard-1d/pch.cpp
+++ Bose-Hubbard-1d/pch.cpp
@@ -1,2 +1,3 @@
-#define ARMA_USE_HDF5 1
+#define ARMA_USE_HDF5 1 //use armadillo h5 load/save
+#define H5_BUILT_AS_DYNAMIC_LIB 1 //use dynamic linking to hdf5 library
 #include "pch.h"
